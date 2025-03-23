9c72196b1be5d2d2c245e9b4b3e8a5421f1a2b60
Arvind Akula <ArvindKumar.Akula@gmail.com>
2025-03-23 00:49:04 -0700

    Add PaymentRequest DTO

---
+++ backend/payment-service/src/main/java/com/ecommerce/paymentservice/dto/PaymentRequest.java
@@ -0,0 +1,42 @@
+package com.ecommerce.paymentservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentRequest {
+    
+    @NotBlank(message = "Order ID is required")
+    private String orderId;
+    
+    @NotBlank(message = "User ID is required")
+    private String userId;
+    
+    @NotNull(message = "Amount is required")
+    @Positive(message = "Amount must be positive")
+    private BigDecimal amount;
+    
+    @NotBlank(message = "Payment method is required")
+    private String paymentMethod;
+    
+    // Additional payment information
+    private String cardNumber;
+    
+    private String cardExpiryMonth;
+    
+    private String cardExpiryYear;
+    
+    private String cardCvv;
+    
+    private String cardHolderName;
+}
