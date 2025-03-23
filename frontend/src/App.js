23c1b353312e1dfe4ed39edbef9b0c92916ca0f6
Arvind Akula <ArvindKumar.Akula@gmail.com>
2025-03-23 00:27:07 -0700

    Add React App component

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ECommerceApp from './components/ECommerceApp';
+
+function App() {
+  return (
+    <div className="App">
+      <ECommerceApp />
+    </div>
+  );
+}
+
+export default App;
