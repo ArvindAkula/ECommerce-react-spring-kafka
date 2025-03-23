f4424a2de83fd0226f3741d7356befebd1426984
Arvind Akula <ArvindKumar.Akula@gmail.com>
2025-03-23 00:30:26 -0700

    Add Kafka topics configuration

---
+++ backend/common/src/main/java/com/ecommerce/common/config/KafkaTopics.java
@@ -0,0 +1,8 @@
+package com.ecommerce.common.config;
+
+public class KafkaTopics {
+    public static final String ORDERS_TOPIC = "orders";
+    public static final String INVENTORY_UPDATES_TOPIC = "inventory-updates";
+    public static final String PAYMENT_EVENTS_TOPIC = "payment-events";
+    public static final String NOTIFICATIONS_TOPIC = "notifications";
+}
